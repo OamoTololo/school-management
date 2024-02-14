ef0d7e4116e85de3625b3b16a4b64f832611a583
Oamogetswe Mgidi <OamogetsweM@miway.co.za>
2024-02-14 11:43:09 +0200

    Created and modified the navbar, included code in the index to call the navbar.

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
     include 'inc/top.php'
 ?>
-
-<button class="btn btn-success">Hello</button>
+<!-- Start of container -->
+<div class="container-fluid">
+    <!-- Start of row -->
+    <div class="row">
+        <!-- Start of col-lg-12 -->
+        <div class="col-lg-12">
+            <?php include ('inc/navbar.php'); ?>
+        </div>
+        <!-- End of col-lg-12 -->
+    </div>
+    <!-- End of row -->
+</div>
+<!-- End of container -->
