64baaf1b241cc022642d2b91b87cc2e0ec2fadce
Oamogetswe Mgidi <OamogetsweM@miway.co.za>
2024-02-27 01:50:29 +0200

    Created pages linking to the navar.

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,31 @@
+<div class="col-md-3 text-white">
+    <h2 class="text-center">Sosha Academy</h2><hr>
+    <p class="text-center">&copy; Oamogetswe Mgidi 2024<a href="www.oamogetswemidi.co.za"> OM</a>, All rights reserved</p>
+</div>
 
+<div class="col-md-3">
+    <h4 class="text-white">Quick Menu</h4><hr>
+    <ul>
+        <li><a href="">Privacy Policy</a></li>
+        <li><a href=""></a>Terms & Conditions</li>
+        <li><a href=""></a>Disclaimer</li>
+        <li><a href=""></a>Contact Us</li>
+    </ul>
+</div>
+
+<div class="col-md-3">
+    <h4 class="text-center text-white">No. of Vistors</h4>
+    <p class="text-center text-white">55</p>
+</div>
+
+<div class="col-md-3 text-white text-center">
+    <h4>Address</h4>
+    <address>
+        498 Block DD Mmutla Street<br>
+        Soshanguve 0152,<br>
+        Gauteng, Pretoria<br>
+    </address>
+</div>
 
 </body>
-
 </html>
--- index.php
+++ index.php
@@ -12,5 +12,15 @@
         <!-- End of col-lg-12 -->
     </div>
     <!-- End of row -->
+
+    <div class="container-fluid">
+        <!-- Start of row -->
+        <div class="row bg-dark mt-2">
+            <!-- Start of col-lg-12 -->
+                <?php include ('inc/footer.php'); ?>
+            <!-- End of col-lg-12 -->
+        </div>
+        <!-- End of row -->
+    </div>
 </div>
 <!-- End of container -->
