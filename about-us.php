7814d12cb6b9ef67c3c2f53cce38acd9c3540bd3
Oamogetswe Mgidi <OamogetsweM@miway.co.za>
2024-02-27 19:43:35 +0200

    Created an about us template using bootstrap card.

---
+++ about-us.php
@@ -0,0 +1,50 @@
+<?php
+    include 'inc/top.php';
+?>
+<div class="container-fluid">
+
+    <div class="row">
+        <div class="col-lg-12 mt-2">
+            <?php include 'inc/navbar.php';?>
+        </div>
+    </div>
+
+    <div class="row mt-2">
+        <div class="col-md-9">
+            <div class="card">
+                <div class="card-header bg-primary text-white">
+                    About Sosha IT Academy
+                </div>
+                    <div class="card-body text-justify">
+                        <p class="card-text">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero qui, voluptatibus. Distinctio eligendi, exercitationem expedita harum, hic illo magnam molestiae obcaecati repellat soluta tempore, temporibus voluptates! Dolore, maiores, quidem. Culpa doloremque eaque quam sint suscipit veniam? Deserunt dolore dolorum eius laboriosam molestias rerum soluta vero vitae. Accusantium aliquid animi architecto aspernatur atque, autem delectus dolorum eius exercitationem harum libero magni mollitia natus nisi nobis praesentium provident quae quas quasi quisquam quo ratione, recusandae sapiente sit temporibus vero voluptas voluptatibus voluptatum!
+                        </p>
+
+                        <p class="card-text">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusamus amet animi at autem consectetur consequuntur dicta dolor dolore doloremque dolores dolorum eaque earum et exercitationem fuga harum hic itaque labore libero magni minima nemo nesciunt nihil, officiis perspiciatis possimus quae quia repellendus saepe sapiente soluta tempore voluptate. Alias commodi consequatur culpa, est laboriosam laborum, numquam odio quia ratione rerum similique sint veritatis, vitae. A commodi culpa cumque, deleniti deserunt dignissimos ipsa iure nesciunt, officiis perferendis praesentium quam quidem reprehenderit similique vitae. Aliquam beatae consequuntur deserunt dignissimos dolor ea error eveniet expedita explicabo, facere fuga fugit illo illum inventore laboriosam magnam magni modi mollitia natus nemo neque nihil numquam obcaecati officiis placeat quam quidem quos rem rerum sint sit soluta tempore temporibus ullam velit veniam voluptate! Ab accusamus asperiores autem debitis delectus dolore dolorum error expedita facilis in ipsam labore minima minus nam natus necessitatibus nostrum, perferendis porro quisquam reiciendis sequi, sint vel. Autem, dolore eaque laboriosam non rerum saepe. Beatae numquam quae quia quos? Ab dolores eum facere magnam quibusdam. Animi, enim eum fuga in molestias quaerat soluta suscipit?
+                        </p>
+
+                        <p class="card-text">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda aut ex explicabo mollitia natus non numquam quod tempore? Minus, nostrum, quod. Atque aut autem cumque dolore dolorem, doloribus esse ex laudantium, magnam nihil omnis praesentium rem ullam ut vel! Accusamus adipisci alias aliquam aliquid asperiores aspernatur assumenda, aut consequatur culpa distinctio dolor error est excepturi impedit in ipsum magni, minima molestiae necessitatibus, officiis pariatur perspiciatis possimus quis rem repellendus reprehenderit sequi sit tempore tenetur voluptatibus. Beatae consequatur dicta, fugit illo laboriosam natus nisi non quam quis ratione rem saepe sed sequi sunt tenetur. At consequuntur dolores eaque laboriosam omnis quo?
+                        </p>
+
+                        <a href="gallery.php" class="btn btn btn-success">View Gallery</a>
+                    </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card bg-primary text-white">
+                <div class="card-body">
+                    <h4 class="card-title text-center">Review</h4>
+                </div>
+            </div>
+            <img src="images/image1.jpg" class="img-fluid">
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <div class="row bg-dark mt-2">
+            <?php include 'inc/footer.php';?>
+        </div>
+    </div>
+</div>
