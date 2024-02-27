9467f8343eaecff343d42e6326b3b8b9ec116cbb
Oamogetswe Mgidi <OamogetsweM@miway.co.za>
2024-02-27 04:52:40 +0200

    Inclded bootstart card into the index page

--- index.php
+++ index.php
@@ -14,6 +14,40 @@
     </div>
     <!-- End of row -->
 
+    <!-- start of row-->
+    <div class="row mt-2">
+        <div class="col-md-9">
+            <h2 class="bg-dark text-white text-center">Wecome to Sosha IT Academy</h2>
+            <p class="text-justify"><b class="text-danger">Sosha IT Academy</b> is set to develop the most dedicated
+                students about IT (Information Technology) Software Development, Our team comes with highly qualified
+                and recognized stuff with sound knowledge of the IT modules. This site was created in 2024 with the
+                purpose to promote higher educational standards. The entire modules are designed to students a solid
+                grounding in front-end, back-end, databases and supply lots of tools and encouragement for students
+                to develop their own approaches. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque quam,
+                totam. Aperiam consectetur doloribus, eveniet ipsa labore laudantium natus neque, nisi optio quae repudiandae
+                tenetur veniam voluptatum. Cupiditate mollitia omnis quod sit? A accusamus adipisci beatae commodi delectus dicta
+                facilis hic in iure molestiae, necessitatibus nesciunt nulla odio odit perspiciatis quia quibusdam, sed sint ullam vel.
+                Ab, culpa eaque, eos error et facilis laudantium magnam minima nam nesciunt officia quisquam, reiciendis repellat tempora
+                voluptates. A accusantium alias, animi architecto aspernatur autem blanditiis consequuntur dolor dolore dolores ducimus enim
+                explicabo illo illum impedit libero maxime nam nemo numquam optio praesentium quia quibusdam, repellat sit tempora ut voluptatum.
+                A accusantium alias aliquam animi atque blanditiis commodi deleniti dicta dignissimos dolorem dolores eligendi exercitationem
+                expedita id illum impedit incidunt itaque, labore laboriosam minima nam natus nemo neque nesciunt officiis optio perferendis
+                perspiciatis, quam quisquam repellendus saepe sapiente ullam vel velit, veritatis vitae voluptate. Aliquam architecto at aut
+                autem commodi delectus dolore ea eum ex fugit ipsa ipsam iure iusto labore laudantium necessitatibus nisi officia officiis
+                omnis quibusdam, rem sit tempora tenetur totam velit veniam voluptatibus? Aperiam autem doloribus eius et fugiat magnam nam
+                optio perspiciatis quae qui quisquam suscipit temporibus unde voluptatem, voluptates!
+            </p>
+        </div>
+        <div class="col-md-3">
+            <div class="card bg-warning text-white">
+                <div class="card-body">
+                    <h4 class="card-title text-center">Parents review</h4>
+                </div>
+            </div>
+        <img src="images/image1.jpg" class="img-fluid">
+        </div>
+    </div>
+    <!-- End of row-->
     <div class="container-fluid">
         <!-- Start of row -->
         <div class="row bg-dark mt-2">
