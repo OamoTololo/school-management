e4fe953d7347832c961f0a1c97d1301b68149a65
Oamogetswe Mgidi <OamogetsweM@miway.co.za>
2024-02-28 12:39:37 +0200

    Designed the front end tamplate for the student page.

--- index.php
+++ index.php
@@ -59,3 +59,6 @@
     </div>
 </div>
 <!-- End of container -->
+</body>
+
+</html>
---
+++ student.php
@@ -0,0 +1,80 @@
+<?php
+include 'inc/top.php';
+?>
+<div class="container-fluid">
+
+    <div class="row">
+        <div class="col-lg-12 mt-2">
+            <?php include 'inc/navbar.php';?>
+        </div>
+    </div>
+
+    <div class="row mt-2">
+        <div class="col-md-9">
+            <small class="text-center text-secondary">
+                <h3>Some of our Successful Students.....</h3>
+            </small><hr>
+
+            <table class="table table-bordered">
+                <thead class="thead-dark">
+                    <tr>
+                        <th>Std No</th>
+                        <th>Student Name</th>
+                        <th>Class</th>
+                        <th>Batch</th>
+                        <th>Image</th>
+                    </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th>001</th>
+                    <th>Tololo</th>
+                    <th>10</th>
+                    <th>10/CBSE/ENG</th>
+                    <th><img src="images/image1.jpg" class="img-fluid" width="100px;" /></th>
+                </tr>
+                <tr>
+                    <th>001</th>
+                    <th>Tololo</th>
+                    <th>10</th>
+                    <th>10/CBSE/ENG</th>
+                    <th><img src="images/image1.jpg" class="img-fluid" width="100px;" /></th>
+                </tr>
+                <tr>
+                    <th>001</th>
+                    <th>Tololo</th>
+                    <th>10</th>
+                    <th>10/CBSE/ENG</th>
+                    <th><img src="images/image1.jpg" class="img-fluid" width="100px;" /></th>
+                </tr>
+                <tr>
+                    <th>001</th>
+                    <th>Tololo</th>
+                    <th>10</th>
+                    <th>10/CBSE/ENG</th>
+                    <th><img src="images/image1.jpg" class="img-fluid" width="100px;" /></th>
+                </tr>
+                </tbody>
+            </table>
+
+        </div>
+        <div class="col-md-3">
+            <div class="card bg-primary text-white">
+                <div class="card-body">
+                    <h4 class="card-title text-center">Review</h4>
+                </div>
+            </div>
+            <img src="images/image1.jpg" class="img-fluid">
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <div class="row bg-dark mt-2">
+            <?php include 'inc/footer.php';?>
+        </div>
+    </div>
+</div>
+
+</body>
+
+</html>
